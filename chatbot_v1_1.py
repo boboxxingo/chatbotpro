d9beddbfcdfdc5220cfe64fd381a1259e63ad71a
boboxxingo <jrespond00@gmail.com>
2023-06-05 07:29:35 -0500

    se modifico el archivo .py

--- chatbot_v1_1.py
+++ chatbot_v1_1.py
@@ -4,7 +4,7 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 # accesar a GPT-3 Por medio de la API
-#openai.api_key = st.secrets["clave"]
+openai.api_key = st.secrets["clave"]
 st.header("Asistente de Soporte Tecnico")
 
  # temp = st.slider("temperature", 0.0, 1.0, 0.5)
